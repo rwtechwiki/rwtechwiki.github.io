a2175808413e48c4c442b98ed9ac890a6d6c186e
shrubfromtomorrow <shrubfromtomorrow@gmail.com>
2025-10-17 12:11:38 -0700

    Quartz sync: Oct 17, 2025, 12:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "rwtechwiki/rwtechwiki.github.io",
+    baseUrl: "rwtechwiki.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
